fc84699ebce0649aedcc5f4e2aa94565b63af140
Joxo19 <95831007+Joxo19@users.noreply.github.com>
2023-10-08 19:11:08 -0600

    Commit

---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    white: "#FFFFFF",
+    black: "#222222",
+    primary: "#EEF4D4",
+    secondary: "#daefb3",
+    grey: "#CCCCCC",
+    colorA: "#1C2826",
+    colorB: "#1B9AAA",
+    colorC: "#1C5253"
+}
+
+export default COLORS;
